c7fad326899a49a7a559a6674dc615876b3beddc
joeryan <joe@clanryan.us>
2012-12-25 02:11:55 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ruby on Rails Tutorial: Demo App
+
+This application demonstrates the rapid prototyping provided by Rails scaffolding.  This application is the scond provided in [*Ruby on Rails Tutorial: Learn Rails by Example*] (http://railstutorial.org)
+by [Michael Hartl] (http://michaelhartl.com/).
+
+edited by Joe Ryan on Decmber 24, 2012.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f81259831a76e04a34d95383a17b8bc13a1be5207c08d73da0b576cd5192957742197356658bcdb34f2ffea26e8d52dfe60266592d5c4c7bcfe8b7976e4e2432'
